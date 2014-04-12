d04ce7a11c55408c970a2346887fc6d76fbecbe3
hzgnjupt <hzgnjupt@gmail.com>
2014-04-12 17:21:14 +0800

    按照顺序存放元素的dict

---
+++ study/hzg/study/ordered_dict.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from collections import OrderedDict
+
+d1 = {}
+d1['foo'] = 1
+d1['bar'] = 2
+d1['spam'] = 3
+d1['grok'] = 4
+print d1
+
+d2 = OrderedDict()
+d2['foo'] = 1
+d2['bar'] = 2
+d2['spam'] = 3
+d2['grok'] = 4
+print d2
